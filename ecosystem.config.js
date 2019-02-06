8bed11ba228f4cdca28e9871e0baf9869d975b1c
John W. Fail <john@pixelache.ac>
2019-02-06 16:09:49 +0200

    ecosystem added

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps : [{
+    name      : 'API',
+    script    : 'app.js',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'deploy',
+      host : 'testing.icewhistle.com',
+      ssh_options: "StrictHostKeyChecking=no",
+      ref  : 'origin/master',
+      repo : 'git@github.com:cenotaph/101api.git',
+      path : '/var/www/testing/api',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
